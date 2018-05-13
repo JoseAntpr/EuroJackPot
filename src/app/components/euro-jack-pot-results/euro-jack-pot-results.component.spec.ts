4c5533800dee9bfe2a1727ae771a904114839696
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-05-13 13:24:32 +0200

    EuroJackPot result components added

---
+++ src/app/components/euro-jack-pot-results/euro-jack-pot-results.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EuroJackPotResultsComponent } from './euro-jack-pot-results.component';
+
+describe('EuroJackPotResultsComponent', () => {
+  let component: EuroJackPotResultsComponent;
+  let fixture: ComponentFixture<EuroJackPotResultsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EuroJackPotResultsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EuroJackPotResultsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/euro-jack-pot-results/euro-jack-pot-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-euro-jack-pot-results',
+  templateUrl: './euro-jack-pot-results.component.html',
+  styleUrls: ['./euro-jack-pot-results.component.scss']
+})
+export class EuroJackPotResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
