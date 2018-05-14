1b7b710f8eb0844f958f7861c2f8a8b35853049f
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-05-14 20:22:31 +0200

    ngOnDestroy added to unsubscribe observables

--- src/app/components/euro-jack-pot-results/euro-jack-pot-results.component.ts
+++ src/app/components/euro-jack-pot-results/euro-jack-pot-results.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EuroJackpotService } from '../../services/euro-jackpot.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-euro-jack-pot-results',
   templateUrl: './euro-jack-pot-results.component.html',
   styleUrls: ['./euro-jack-pot-results.component.scss']
 })
-export class EuroJackPotResultsComponent implements OnInit {
+export class EuroJackPotResultsComponent implements OnInit, OnDestroy {
 
   euroJackPot;
+  subscription: Subscription;
 
   constructor(private euroJackpotService: EuroJackpotService) {}
 
   ngOnInit() {
-    this.euroJackpotService.latestWinnerNumber().subscribe( (res: any) => {
+    this.subscription = this.euroJackpotService.latestWinnerNumber().subscribe( (res: any) => {
       console.log(res);
       this.euroJackPot = res.last;
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
